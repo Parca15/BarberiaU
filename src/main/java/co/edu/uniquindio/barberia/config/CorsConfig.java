0fe1d24c12c91a1aeb01a0d0e0fcf4a74a208ecd
Parca15 <142187202+Parca15@users.noreply.github.com>
2025-11-06 07:16:30 -0500

    version anterior del sprint2
    la sincronización con el fornted presento error, por eso se vuelve a la version anterior

--- src/main/java/co/edu/uniquindio/barberia/config/CorsConfig.java
+++ src/main/java/co/edu/uniquindio/barberia/config/CorsConfig.java
@@ -2,23 +2,24 @@ package co.edu.uniquindio.barberia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8080")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/api/**", config);
+        return new CorsFilter(source);
     }
 }
--- src/main/resources/static/static/js/488.f03e70d8.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkbarberia_frontend=self.webpackChunkbarberia_frontend||[]).push([[488],{488:(e,a,n)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.f03e70d8.chunk.js.map
