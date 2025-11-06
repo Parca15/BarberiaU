acf43806591fc31269bb79a4465159c94d9b6fc9
Parca15 <142187202+Parca15@users.noreply.github.com>
2025-11-05 20:54:39 -0500

    arreglo 2

---
+++ src/main/resources/static/static/js/488.f03e70d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkbarberia_frontend=self.webpackChunkbarberia_frontend||[]).push([[488],{488:(e,a,n)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.f03e70d8.chunk.js.map
